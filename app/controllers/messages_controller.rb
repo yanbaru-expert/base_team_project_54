1fcbe917d3cb11888d7c9c426a4e8e9e835a5d2c
motokitakashima <pageboy.jr@icloud.com>
2021-02-17 23:12:06 +0900

    削除機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,6 +25,11 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
   end
 
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy
+  end
+
   private
 
   def message_params
